ac618e7bdf6b5f25d6fa4949d66864fb6b9ef4b2
Akshar Raaj <akshar@agiliq.com>
2015-02-25 19:48:48 +0530

    Changing the slice changes the underlying array

--- slices.go
+++ slices.go
@@ -2,10 +2,31 @@ package main
 
 import "fmt"
 
+func change_slice(slice []int) {
+	//Slice only points to internal array.
+	//So the passed array will be changed and change will
+	//be reflected in the passed slice
+	slice[0] = 50
+}
+
 func main() {
 	arr := make([]int, 2)
+	arr[0] = 5
+	arr[1] = 3
 	fmt.Println(arr[0])
 	fmt.Println(arr[1])
 	arr = append(arr, 2)
+
+	change_slice(arr)
+	fmt.Println("*******")
+	fmt.Println(arr[0])
+	fmt.Println(arr[1])
+	fmt.Println(arr[2])
+	new_arr := arr[1:3]
+	
+	change_slice(new_arr)
+	fmt.Println("*******")
+	fmt.Println(arr[0])
+	fmt.Println(arr[1])
 	fmt.Println(arr[2])
 }
